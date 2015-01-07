a328436c2e9f25bce1207af4eb4f293251f4f5d7
Bernt Danielsen <bernt.danielsen@gmail.com>
2015-01-07 03:25:24 +0100

    Test

---
+++ webtest.js
@@ -0,0 +1,11 @@
+/**
+ * @author bernt
+ * test av Aptana Studio 3
+ */
+var test1="testverdi";
+function testFunksjon(){
+	console.log("Skrevet fra JS:" + test1);
+	document.write("Denne sidens tittel er: " + document.title);	
+}
+
+
